d034f4017d8b2d6e12300ce46cee5809485ca94e
j32u4ukh <j32u4ukh@gmail.com>
2022-04-16 09:32:26 +0800

    最低限度滿足所需機能

---
+++ Assets/SpeechManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeechManager : MonoBehaviour
+{
+    public FantomLib.TextToSpeechController controller;
+    public Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller.OnStatus.AddListener(OnStatus);
+
+        button.onClick.AddListener(()=> 
+        {
+            Debug.Log($"Locale: {controller.Locale}");
+            controller.StartSpeech("こんねねー 五期生オレンジ担当、桃鈴ねねです");
+        });
+    }
+
+    public void OnStatus(string message)
+    {
+        Debug.Log($"[SpeechManager] OnStatus | message: {message}");
+    }
+}
